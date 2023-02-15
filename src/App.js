8e286ee611232cff6d36ddaa9674be722ab5b0a2
AliSalih1995git <kvaliswalihtnl@gmail.com>
2023-02-15 12:49:03 +0530

    Frontent UI completed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
-import './App.css';
+import "./App.css";
+import Header from "./components/Header";
+import Output from "./components/Output";
 
 function App() {
   return (
     <div className="App">
-    <h1>Hello</h1>
+      <Header />
+      <div className="container">
+        <form>
+          <table>
+            <tr className="fields">
+              <th>First Number:</th>
+              <td>
+                <input type="text" />
+              </td>
+            </tr>
+            <tr className="fields">
+              <th>Second Number:</th>
+              <td>
+                <input type="text" />
+              </td>
+            </tr>
+            <tr className="fields">
+              <th></th>
+              <td>
+                <button>Generate Steps</button>
+              </td>
+            </tr>
+          </table>
+        </form>
+        <Output />
+      </div>
     </div>
   );
 }
---
+++ src/components/Output.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Output() {
+  return (
+    <div className="output_container">
+      <div className="output_screen">Output</div>
+    </div>
+  );
+}
+
+export default Output;
